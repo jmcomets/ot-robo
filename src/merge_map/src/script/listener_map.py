629c4efe8657a2fc9adddfade0daa04aafbb1639
Celendil <celendil@celendil-E7003.(none)>
2015-03-11 14:41:53 +0100

    test listener map

--- src/merge_map/src/script/listener_map.py
+++ src/merge_map/src/script/listener_map.py
@@ -1,1 +1,18 @@
 
+#!/usr/bin/env python
+import rospy
+from nav_msgs.msg import OccupancyGrid
+
+
+def callback(data):
+    rospy.loginfo(rospy.get_name() + ": I heard %s" % data.data)
+
+
+def listener():
+    rospy.init_node('listener_map', anonymous=True)
+    rospy.Subscriber("map", OccupancyGrid, callback)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    listener()
