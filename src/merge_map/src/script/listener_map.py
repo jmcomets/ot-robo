070db6c6a1c5552b4321d864f6be6097fef47edf
Celendil <celendil@celendil-E7003.(none)>
2015-03-11 15:49:03 +0100

    merge listener et writer works

--- src/merge_map/src/script/listener_map.py
+++ src/merge_map/src/script/listener_map.py
@@ -1,15 +1,16 @@
-
 #!/usr/bin/env python
 import rospy
 from nav_msgs.msg import OccupancyGrid
 
-
+rospy.init_node('listener_map', anonymous=True)
+pub = rospy.Publisher('broadcastMap', OccupancyGrid)
+    
 def callback(data):
-    rospy.loginfo(rospy.get_name() + ": I heard %s" % data.data)
+    rospy.loginfo(rospy.get_name() + ": I published on broadcastMap")
+    pub.publish(data)
 
 
 def listener():
-    rospy.init_node('listener_map', anonymous=True)
     rospy.Subscriber("map", OccupancyGrid, callback)
     rospy.spin()
 
