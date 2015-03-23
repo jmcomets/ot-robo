a0757c76fdaf5b4e845e69f932e99ff2eabac033
turtlebot <turtlebot@turtlebot-SATELLITE-NB10-A-103.(none)>
2015-03-23 10:41:44 +0100

    Yoloooo ça maaaaaaarche !!!

--- src/slam_strategy/src/motion.py
+++ src/slam_strategy/src/motion.py
@@ -3,11 +3,10 @@ import rospy
 from geometry_msgs.msg import Twist
 
 # constants
-min_rotation = math.pi / 3
-max_rotation = math.pi / 2
+min_rotation = math.pi / 8
+max_rotation = math.pi / 4
 min_translation = 0.08
 max_translation = 0.25
-twist_duration = 1
 
 def fragment_moves(value, min_value, max_value):
     """For a range of values [min, max] as well as a requested value, return
@@ -16,14 +15,17 @@ def fragment_moves(value, min_value, max_value):
     """
     rospy.logdebug('request = %s, min = %s, max = %s', value, min_value, max_value)
     values = []
-    while value >= min_value:
-        delta = min(value, max_value)
+    while abs(value) >= abs(min_value):
+        delta = min(abs(value), abs(max_value))*sign(value)
         values.append(delta)
         value -= delta
     rospy.logdebug('values = %s, error = %s', values, value)
     return values, value
 
-def follow_path(path, orientation):
+def sign(value):
+    return value/abs(value)
+
+def follow_path(path, orientation, resolution):
     motion_pub = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist)
     rotation_error = 0
     translation_error = 0
@@ -33,14 +35,21 @@ def follow_path(path, orientation):
 
     for i in range(1, len(path)):
         rospy.logdebug('moving to new waypoint')
+        rospy.logdebug('current position is : %s', path[i])
+        rospy.logdebug('current cell is : (%s, %s)', path[i][0] / resolution, path[i][1] / resolution)
 
         # rotation
         rotation_twist = Twist()
         angular_dist = math.atan2(path[i][1] - path[i - 1][1],
                                   path[i][0] - path[i - 1][0])
         angular_dist += rotation_error
+        if angular_dist > math.pi:
+            angular_dist = - angular_dist % math.pi
         if angular_dist != 0:
-            moves, rotation_error = fragment_moves(angular_dist, min_rotation, max_rotation)
+            if angular_dist > 0:
+                moves, rotation_error = fragment_moves(angular_dist, min_rotation, max_rotation)
+            else:
+                moves, rotation_error = fragment_moves(angular_dist, -max_rotation, -min_rotation)
             for rotation in moves:
                 rotation_twist.angular.z = rotation
                 rospy.logdebug('rotating by %s radians', rotation_twist.angular.z)
